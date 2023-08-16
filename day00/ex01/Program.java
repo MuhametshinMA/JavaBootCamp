9090ccfbc7fbcee35f07d5dbe243bee5aeca44d7
Little Carlena <littleca@si-c7.nsk.21-school.ru>
2023-08-16 16:22:26 +0700

    fix ex01, ex02 done

--- day00/ex01/Program.java
+++ day00/ex01/Program.java
@@ -1,25 +1,13 @@
 import java.util.Scanner;
 
 public class Program {
-  public static boolean checkPrimalNumber(int number) {
-    if (number < 2) {
-      System.out.println("IllegalArgument");
-      System.exit(1);
-    }
-    for (int i = 2; i < number / 2; i++) {
-      if (number % i == 0) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+    System.out.print("-> ");
     int number = input.nextInt();
-
+    input.close();
     if (number < 2) {
-      System.out.println("-> " + number);
+
       System.out.println("   IllegalArgument");
       System.exit(1);
     }
@@ -35,7 +23,6 @@ public class Program {
       }
     }
 
-    System.out.println("-> " + number);
     System.err.println("   " + isPrimal + " " + countLoop);
 
   }
